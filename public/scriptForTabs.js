786b6c495fcb219acba7a6538f1f172f1ba5f4bb
Nguyen Cong Danh <no1st.skipper@gmail.com>
2017-11-21 07:52:15 +0200

    Add UI for Add Item

--- public/scriptForTabs.js
+++ public/scriptForTabs.js
@@ -12,3 +12,26 @@
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+function openAddItemsTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabAddItem");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabAddItemlinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+$('.dropdown-toggle').click(function (e) {
+    e.preventDefault();
+    setTimeout($.proxy(function () {
+        if ('ontouchstart' in document.documentElement) {
+            $(this).siblings('.dropdown-backdrop').off().remove();
+        }
+    }, this), 0);
+});
